fb0da28adb60444ae304bd431a969b9420cc16c1
rgomesss <rgomessss16@gmail.com>
2024-12-29 19:25:36 -0300

    Updates

--- src/hooks/useJogoDaMemoria.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { LogicaJogoDaMemoriaContext } from "../contexts/LogicaJogoDaMemoria";
-
-export const useJogoDaMemoria = () => {
-  const contexto = useContext(LogicaJogoDaMemoriaContext);
-  return contexto;
-};
