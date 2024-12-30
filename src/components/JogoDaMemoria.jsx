affeddd9306c4b216a4b7d2d7fceeddb3104b049
rgomesss <rgomessss16@gmail.com>
2024-12-30 01:25:36 -0300

    Descrição das alterações feitas

--- src/components/JogoDaMemoria.jsx
+++ src/components/JogoDaMemoria.jsx
@@ -24,6 +24,7 @@ const JogoDaMemoriaConteudo = () => {
     <div className="jogo-da-memoria">
       <div className="jogo-da-memoria__conteudo">
         <h1>Jogo da patt@patt</h1>
+        <h5>Criado por Rafael Gomes</h5>
         <Placar />
         {cartas?.length === 0 ? (
           <p>Carregando cartas...</p>
